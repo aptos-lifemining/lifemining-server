ebb08fee6033d6dd0238cbe1844bbd1ed0102e69
yj990315 <yj990315@gmail.com>
2023-01-31 13:09:06 +0900

    feat: typeorm 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), // TODO: options 지정
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true, // include all entities
+      synchronize: process.env.DB_SYNCHRONIZE === 'true', // 개발 환경에서만 true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
